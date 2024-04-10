b1c817eb66a7d2c47a1087bb67f9e7c65eca9090
AguilarJ11 <8664@holbertonstudents.com>
2024-04-10 15:13:00 +0000

    selection_sort done

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ *selection_sort - apply selection sort for an array
+ *@array: entry int array
+ *@size: entry array size
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t c1, c2, pos, aux;
+
+	for (c1 = 0; c1 < size; c1++)
+	{
+		pos = c1;
+
+		for (c2 = c1; c2 < size; c2++)
+		{
+			if (array[c2] < array[pos])
+			{
+				aux = array[c2];
+				pos = c2;
+				printf("Valor Pos:%i\n", array[c2]);
+			}
+		}
+		if (pos != c1)
+		{
+			array[pos] = array[c1];
+			array[c1] = aux;
+			print_array(array, size);
+		}
+	}
+}
